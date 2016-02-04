02b4117b91f2daed32a951635f4be2194d9187b7
ajohn2550 <alex@johnsonwebware.com>
2016-02-03 21:05:30 -0700

    Index to update hosts now

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+global.config = require('./config/production.json');
+
+var getHosts = requir('./lib/git-hosts');
+var updateHosts = require('./lib/update-hosts');
+
+updateHosts(global.config.hostsFile, global.config.ip, getHosts(global.config.sources, (err) => {
+	if (err) {
+		console.error(err);
+	} else {
+		console.log('Updated Hosts');
+	}
+}));
