52317803831e469a41ea8141c2754c35f6d1d85b
ajohn2550 <alex@johnsonwebware.com>
2016-02-04 20:30:05 -0700

    fixed index.js to call update hosts after get hosts completes.

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ global.config = require('./config/production.json');
 var getHosts = require('./lib/get-hosts');
 var updateHosts = require('./lib/update-hosts');
 
-updateHosts(global.config.hostsFile, global.config.ip, getHosts(global.config.sources, (err) => {
-	if (err) {
-		console.error(err);
-	} else {
-		console.log('Updated Hosts');
-	}
-}));
+getHosts(global.config.sources, (err, blockHosts) => {
+	updateHosts(global.config.hostsFile, global.config.ip, blockHosts, (err) => {
+		if (err) {
+			console.error(err);
+		} else {
+			console.log('Updated Hosts');
+		}
+	});
+});
